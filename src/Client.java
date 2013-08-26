cf602ca5fa4f771d5fbeab3cd576d7cc123a5321
Mezhensky <mezhenskydb@mail.ru>
2013-08-26 14:15:17 +0400

    Update Client.java
    Remove program params out of code

--- src/Client.java
+++ src/Client.java
@@ -2,12 +2,12 @@ import java.net.*;
 import java.io.*;
 
 public class Client {
-    private static int port = 3553;
+    private static int port = 3553;                                         //params are hard-coded
     public static void main(String[] ar) {
         Socket socket;
         try {
             System.out.println("Сlient starting.");
-            socket = new Socket("localhost", port);
+            socket = new Socket("localhost", port);                         //params are hard-coded
             System.out.println("Сonnection to the server was successful.");
             Thread threadc = new Thread(new ClientImpRun(socket));
             threadc.start();
